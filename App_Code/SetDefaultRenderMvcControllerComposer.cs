65018e26d6a119040834fadfefd766c41311ae2f
joshbm-git <32262804+joshbm-git@users.noreply.github.com>
2021-10-04 09:45:59 +0200

    Added localdb

---
+++ App_Code/SetDefaultRenderMvcControllerComposer.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+
+namespace AarhusWebDevCoop.App_Code
+{
+    public class SetDefaultRenderMvcControllerComposer : IUserComposer 
+    {
+        // GET: SetDefaultRenderMvcControllerComposer
+        public void Compose(Composition composition)
+        {
+            RouteTable.Routes.MapUmbracoRoute(
+                "ProjectsCustomRoute",
+                "all-projects/{status}",
+                new
+                {
+                    controller = "ProjectsOverview",
+                    action = "index",
+                    status = UrlParameter.Optional
+                },
+                //Todo change number to correct
+                new UmbracoVirtualNodeByIdRouteHandler(1186));
+               
+        }
+    }
+}
